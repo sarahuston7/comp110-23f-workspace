50feee4fc5a5b9d2528676d94756058e9e8ddd75
Sara Huston <shuston@unc.edu>
2023-11-09 10:27:56 -0500

    class

--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -10,10 +10,15 @@ class Point:
     x: float
     y: float
 
-    def __init__(self, x_init: float, y_init: float):
+    def __init__(self, x_init: float = 0.0, y_init: float = 0.0):
         """My constructor."""
         self.x = x_init
         self.y = y_init
+    
+    def __str__(self):
+        """Printing method."""
+        output: str = f"x: {self.x}; y: {self.y}"
+        return output
 
     def scale_by(self, factor: int):
         """Mutates a coordinate."""
@@ -24,3 +29,23 @@ class Point:
         """Creates a new scaled point."""
         new_point: Point = Point(self.x * factor, self.y * factor)
         return new_point
+    
+    def __mul__(self, factor: int | float) -> Point:
+        """Multiplies old point and creates new."""
+        new_point: Point = self
+        new_point.x *= factor
+        new_point.y *= factor
+        return new_point
+    
+    def __add__(self, addn: int | float) -> Point:
+        """Adds a number onto old point into new point."""
+        added_point: Point = self
+        added_point.x += addn
+        added_point.y += addn
+        return added_point
+
+
+sara_point: Point = Point(3.0, 7.0)   
+print(sara_point)
+fun_point: Point = sara_point + 3.0
+print(fun_point)
