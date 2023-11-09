71993c51d5dfdad78a44297b16b056e462afbdc6
Sara Huston <shuston@unc.edu>
2023-11-09 12:08:11 -0500

    idk

--- exercises/ex08/bear.py
+++ exercises/ex08/bear.py
@@ -9,16 +9,6 @@ class Bear:
         "Constructing a bear."
         self.age = age2
         self.hunger_score = hunger_score2
-    
-    # def __str__(self):
-    #     """Printing method."""
-    #     output: str = f"Age of bear: {self.age}\n"
-    #     output += f"Hunger score of bear: {self.hunger_score}"
-    #     return output
 
     def one_day(self):
         return None
-    
-
-# sara_bear: Bear = Bear(20,10)
-# print(sara_bear)
--- exercises/ex08/river_simulation.py
+++
@@ -1,6 +0,0 @@
-"""Creating a river simulation."""
-
-from exercises.ex08 import River
-
-my_river: River = River(10,2)
-my_river.view_river()
